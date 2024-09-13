955cae223c8dcda3e43ed39ae76d7674267b98f8
breshinas <26814232+breshinas@users.noreply.github.com>
2024-09-13 10:43:05 +0200

    build 1.5.4

--- build/src/nodes/amqp-out.js
+++ build/src/nodes/amqp-out.js
@@ -75,7 +75,9 @@ module.exports = function (RED) {
                         channel.removeAllListeners();
                         channel.close()
                             .catch(err => {
-                            me.error('Error closing channel:', err);
+                            if (err && !(err.length && err.length == 0))
+                                me.error('Error closing channel:', err);
+                            // otherwise silently drop non-informative error
                         });
                         //channel = null;
                     }
